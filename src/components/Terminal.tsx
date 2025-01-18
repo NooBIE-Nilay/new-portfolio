ad824ed534d68effd9023a88dfa96c0924d4366e
Nilay Banerjee <n00bie@Nilays-MacBook-Air.local>
2025-01-19 03:11:33 +0530

    Fixed Terminal Input Field CSS

--- src/components/Terminal.tsx
+++ src/components/Terminal.tsx
@@ -6,29 +6,27 @@ import Resume from "./TerminalCommands/Resume";
 import Education from "./TerminalCommands/Education";
 import Skills from "./TerminalCommands/Skills";
 import Interests from "./TerminalCommands/Interests";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Terminal() {
-  function executeCommand(command: string) {}
-  // function checkCommand(command: string) {
-  //   let val = command.trim();
-  //   let RegEx = /^[a-z0-9]+$/i;
-  //   if (RegEx.test(val)) return <div>{command}</div>;
-  //   return "";
-  // }
-  function validateInput(key: string) {
-    let val = key.trim();
-    let RegEx = /^[a-z0-9]+$/i;
-    if (!RegEx.test(val) && key !== "Enter") {
-      console.log("Error!");
+  function handleCommand() {
+    setCommand(command.trim());
+    let RegEx = /^[a-z0-9.]+$/i;
+    if (!RegEx.test(command) && command !== "Enter") {
+      console.log("Error Parsing Command");
       return;
     }
-    if (key !== "Enter") executeCommand(command);
-    else setCommand((command) => command + key);
+    console.log("Command", command);
+
+    setCommand("");
   }
+  const [isFocused, setIsFocused] = useState(true);
   const [command, setCommand] = useState("");
+  const inputRef = useRef(null);
+
   return (
-    <div className="terminal">
+    //@ts-ignore
+    <div className="terminal" onClick={() => inputRef.current?.focus()}>
       <div className="terminal-header bg-terminal-header h-7 flex justify-start items-center rounded-t-lg ">
         <div className="header-button red bg-[#f96256] w-3 h-3 rounded-full ml-3 border-1 border-solid border-[#f65549]"></div>
         <div className="header-button red border-[#fdbc3d] w-3 h-3 rounded-full ml-2 border-1 border-solid broder-1 bg-[#ffb524]"></div>
@@ -36,6 +34,9 @@ export default function Terminal() {
       </div>
       <div className="terminal-window bg-terminal rounded-b-md  p-9 text-white">
         <div>
+          &gt; {data.first_name}.skills
+          <Skills />
+          <br />
           &gt; {data.first_name}.contactInfo
           <ContactInfo />
           <br />
@@ -48,23 +49,28 @@ export default function Terminal() {
           &gt; {data.first_name}.education
           <Education />
           <br />
-          &gt; {data.first_name}.skills
-          <Skills />
-          <br />
           &gt; {data.first_name}.interests
           <Interests />
         </div>
         <br />
-        {/* <input
-          className="w-auto bg-terminal text-white"
-          value={command}
-          onChange={(e) => setCommand(e.target.value)}
-          autoFocus
-          placeholder=""
-          /> */}
-        <div className="relative ">
+        <div className="flex w-auto">
           &gt;
-          <div></div>
+          <input
+            ref={inputRef}
+            className={`bg-terminal text-white  caret-terminal  outline-none border-none focus:outline-none focus:ring-0 ml-2`}
+            style={{ width: `${command.length * 9.8}px` }}
+            value={command}
+            onChange={(e) => setCommand(e.target.value)}
+            autoFocus
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                handleCommand();
+              }
+            }}
+            onFocus={(e) => setIsFocused(true)}
+            onBlur={(e) => setIsFocused(false)}
+          />
+          <i className={`bg-white/80 ${isFocused && "animate-blink"} w-3 `}></i>
         </div>
       </div>
     </div>
