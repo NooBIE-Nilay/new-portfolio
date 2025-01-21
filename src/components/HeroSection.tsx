cce655b8d9852f49ca504d3fa53f15e32faffc23
Nilay Banerjee <n00bie@Nilays-MacBook-Air.local>
2025-01-21 19:38:56 +0530

    Made HeroSection Responsive

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -4,9 +4,9 @@ import TypingAnimation from "./ui/typing-animation";
 export default function Hero() {
   return (
     <div className="hero-container py-10">
-      <div className="hero-title text-accent text-6xl pb-16 flex items-center">
-        <span className="mx-4 flex ">&gt;</span>
-        <TypingAnimation duration={75} className="text-6xl ">
+      <div className="hero-title text-accent  pb-8  md:pb-16 flex items-start md:items-center">
+        <span className="mx-4 flex font-semibold text-5xl">&gt;</span>
+        <TypingAnimation duration={75} className="text-5xl md:text-7xl ">
           {data.name}
         </TypingAnimation>
       </div>
